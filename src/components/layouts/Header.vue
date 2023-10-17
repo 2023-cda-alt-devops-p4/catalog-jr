f8d09e7e8575430b46cd95dbdef744e924c7a8d4
Edelbjorn <jeremy.rouviller@nils-app.com>
2023-10-17 10:55:17 +0200

    fix-feat/Old files deletions + add header file

---
+++ src/components/layouts/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header">
+    <div class="navigation">
+      Hello World
+    </div>
+  </div>
+</template>
+
+
+<script setup lang="ts">
+
+</script>
+
+
+<style scoped lang="scss">
+.header {
+  height: 70px;
+  width: 100%;
+  background-color: red;
+}
+</style>
